9976cf9dfd932badbd805e434f4cb1775a04b4ac
HolyGeek404 <wmatkowski404@proton.me>
2025-08-05 15:17:08 +0200

    Added Gpu, Cpu and Cooler model

---
+++ GoodStuff_DomainModels/Models/Products/Cooler.cs
@@ -0,0 +1,33 @@
+namespace Models.Products;
+
+public class Cooler
+{
+    public string Name { get; set; }
+    public string Team { get; set; }
+    public string CoolerType { get; set; }
+    public string Compatibility { get; set; }
+    public string Size { get; set; }
+    public string HeatPipes { get; set; }
+    public string Fans { get; set; }
+    public string RPMControll { get; set; }
+    public string RMP { get; set; }
+    public string BearingType { get; set; }
+    public string FanSize { get; set; }
+    public string Connector { get; set; }
+    public string SupplyVoltage { get; set; }
+    public string SupplyCurrent { get; set; }
+    public string Highlight { get; set; }
+    public string MTBFLifetime { get; set; }
+    public string Height { get; set; }
+    public string Width { get; set; }
+    public string Depth { get; set; }
+    public string Weight { get; set; }
+    public string Warranty { get; set; }
+    public string ProducentCode { get; set; }
+    public string ProductImg { get; set; }
+    public string Price { get; set; }
+    public string Manufacture { get; set; }
+    public string Category { get; set; }
+    public string ProductId { get; set; }
+    public string Id { get; set; }
+}
---
+++ GoodStuff_DomainModels/Models/Products/Gpu.cs
@@ -0,0 +1,32 @@
+namespace Models.Products;
+
+public class Gpu
+{
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public string Team { get; set; }
+    public string GpuProcessorLine { get; set; }
+    public string PCIeType { get; set; }
+    public string MemorySize { get; set; }
+    public string MemoryType { get; set; }
+    public string MemoryBus { get; set; }
+    public string MemoryRatio { get; set; }
+    public string CoreRatio { get; set; }
+    public string CoresNumber { get; set; }
+    public string CoolingType { get; set; }
+    public string OutputsType { get; set; }
+    public string SupportedLibraries { get; set; }
+    public string PowerConnector { get; set; }
+    public string RecommendedPSUPower { get; set; }
+    public string Length { get; set; }
+    public string Width { get; set; }
+    public string Height { get; set; }
+    public string Warranty { get; set; }
+    public string ProducentCode { get; set; }
+    public string PGPCode { get; set; }
+    public string GpuProcessorName { get; set; }
+    public string Manufacturer { get; set; }
+    public string Category { get; set; }
+    public string ProductId { get; set; }
+    public string ProductImg { get; set; }  
+}
